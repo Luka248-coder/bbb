ca56028582776933c190a243dc25f60f1b336f0b
Luka248-coder <blukab1.140@icloud.com>
2026-06-05 12:10:35 +0200

    Update page.tsx

--- app/request/page.tsx
+++ app/request/page.tsx
@@ -1,31 +1,22 @@
 export const dynamic = 'force-dynamic'
-
 import { redirect } from 'next/navigation'
 import { getSession } from '@/lib/auth'
-import { Navbar } from '@/components/navbar'
 import { Footer } from '@/components/footer'
 import { RequestForm } from '@/components/request-form'
 
 export const metadata = {
   title: 'Demander un contenu - Streamself',
-  description: 'Demandez l\'ajout d\'un film ou d\'une série sur Streamself',
+  description: "Demandez l'ajout d'un film ou d'une série sur Streamself",
 }
 
 export default async function RequestPage() {
   const user = await getSession()
-
-  if (!user) {
-    redirect('/login')
-  }
-
+  if (!user) redirect('/login')
   return (
     <div className="min-h-screen flex flex-col bg-background">
-      <Navbar />
-      
       <main className="flex-1 pt-24">
         <RequestForm userId={user.id} />
       </main>
-      
       <Footer />
     </div>
   )
