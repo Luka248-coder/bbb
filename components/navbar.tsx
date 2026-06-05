ac9a9c0ced22d3657becf633169a5b3e27a3dbb3
Luka248-coder <blukab1.140@icloud.com>
2026-06-05 12:01:49 +0200

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -292,7 +292,7 @@ export function Navbar() {
   }
 
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 pointer-events-none">
+    <header className="fixed top-8 left-0 right-0 z-50 pointer-events-none">
 
       <div className="relative flex items-center h-[60px] px-4 md:px-6">
 
